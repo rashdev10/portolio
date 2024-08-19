6f91a241ff65514eb6175ed6ea3720ef6157d610
Rasheed Iskilu <riskilu@plateaumed.com>
2024-08-19 07:35:45 +0100

    completed-porfolio

---
+++ data/index.ts
@@ -0,0 +1,258 @@
+export const navItems = [
+  {name: "About", link: "#about"},
+  {name: "Projects", link: "#projects"},
+  {name: "Testimonials", link: "#testimonials"},
+  {name: "Contact", link: "#contact"},
+]
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+]
+
+export const projects = [
+  {
+    id: 1,
+    title: "Transform hospital operations",
+    des: "Neo EHR provides you with a 360 degree view: the most relevant data you need for clinical care, growing revenues and managing pharmacy inventory",
+    img: "/plateaumedImage.svg",
+    iconLists: [
+      "/next.svg",
+      "/net.svg",
+      "/postgres.svg",
+      "/ts.svg",
+      "/redux.svg",
+    ],
+    link: "https://www.plateaumed.com/",
+  },
+  {
+    id: 2,
+    title: "Jetron - Finding & Attending events",
+    des: "A platform for you to discover, share and promote your events, Sell your event tickets here!",
+    img: "/jetron.svg",
+    iconLists: [
+      "/next.svg",
+      "/chakra-ui.svg",
+      "/ts.svg",
+      "/node-js.svg",
+      "/postgres.svg",
+    ],
+    link: "https://www.jetronticket.com/",
+  },
+  {
+    id: 3,
+    title: "Girls 4 Leadership",
+    des: "Girls 4 Leadership Initiative (G4L) imbibes a culture of inclusion which allows us to engage boys and men in our programs and projects.",
+    img: "/leadership.svg",
+    iconLists: ["/next.svg", "/airtable.svg", "/ts.svg", "/redux.svg"],
+    link: "https://girls4leadership.org/",
+  },
+  {
+    id: 4,
+    title: "Buy and sell music, albums",
+    des: "Discover new music, connect with musicians, and grow your network",
+    img: "/mixcurb.png",
+    iconLists: ["/vuejs.svg", "/ts.svg", "/node-js.svg", "/tail.svg"],
+    link: "https://mixcurb.com/",
+  },
+  {
+    id: 5,
+    title: "Digital wallet for tax-free shopping",
+    des: "Samar is your automated travel wallet. We use customized digital solutions to simplify your travel and retail experiences",
+    img: "/samarweb.svg",
+    iconLists: [
+      "/gatsbyjs.svg",
+      "/javascript.svg",
+      "/airtable.svg",
+      "/css.svg",
+    ],
+    link: "https://www.trysamar.com/",
+  },
+  {
+    id: 6,
+    title: "Towards the Isolo of our Dreams",
+    des: "Official website of ISOLO LCDA, Where we keep you abreast of the Latest Happenings and Development in the LCDA",
+    img: "/isololcda.svg",
+    iconLists: [
+      "/gatsbyjs.svg",
+      "/javascript.svg",
+      "/airtable.svg",
+      "/sass.svg",
+    ],
+    link: "https://isololcda-raashot12.vercel.app/",
+  },
+]
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Rasheed was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rasheed's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rasheed is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Rasheed was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rasheed's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rasheed is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Rasheed was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rasheed's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rasheed is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Rasheed was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rasheed's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rasheed is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Rasheed was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rasheed's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rasheed is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+]
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+]
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer at Plateaumed",
+    desc: "Contributed in the development of a web-based and both iOS & Android platforms using React Native and Next.js",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Frontend Engineer at Jetron Mall",
+    desc: "Designed and developed web-based platform using Next.js,Tyepscript, and Chakra UI to enhancing robust user experience",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Frontend Engineer at Samar",
+    desc: "Led the dev of a microsite website using gatsby and airtable and deployed to firebase hosting.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Frontend Engineer at Canary Point Services",
+    desc: "Developed and maintained user-facing features using modern frontend technologies such as React, Typescript and Strapi",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+]
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Raashot12",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/rashdev_i",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/rasheed-dev/",
+  },
+]
